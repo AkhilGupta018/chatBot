14b1aa433e5c215cc54bbbe0be534555c4a22b77
akhil <akgupta1337@gmail.com>
2025-12-11 17:49:06 +0530

    edited

--- backend.py
+++ backend.py
@@ -3,7 +3,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from typing import Annotated, TypedDict
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages, BaseMessage
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, SystemMessage
 # from dotenv import load_dotenv
 import os
 
@@ -33,10 +33,27 @@ graph.add_edge('chatNode', END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
 
-def askBot(user_input):
-    final_state = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG) # type: ignore
-    return final_state['messages'][-1].content
+DEFAULT_PROMPT = """
+You are a helpful assistant.
+The owner's name is Akhil the Boss.
+You are created by him.
 
+About him:
+he owns 6 million dollars
+he is god of 9 planets
+"""
+
+def askBot(user_input):
+    final_state = chatbot.invoke(
+        {
+            "messages": [
+                SystemMessage(content=DEFAULT_PROMPT),
+                HumanMessage(content=user_input)
+            ]
+        },
+        config=CONFIG # type: ignore
+    )
+    return final_state["messages"][-1].content
 
 if __name__ == '__main__':
     print(askBot("my name akhil"))
